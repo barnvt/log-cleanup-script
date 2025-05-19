17f5dc5c393859043d7e2485212779e05c65af64
barnvt <baranvtw@gmail.com>
2025-05-19 16:29:41 +0300

    script updated to archive and delete log files

--- cleanup.sh
+++ cleanup.sh
@@ -7,20 +7,28 @@ read -p "Enter the path to the log directory: " log_dir
 if [[ -d "$log_dir" ]]; then
 
     # Find .log files in the directory
-    log_files=$(find "$log_dir" -type f -name "*.log")
+    log_files=$(find "$log_dir" -mtime +7 -type f -name "*.log")
 
     # Check if any .log files were found
     if [[ -n "$log_files" ]]; then
 
-        echo "Directory exists and contains .log files."
         echo "$log_files"
 
         # Get confirmation from the user to delete the files
         read -p "Do you want to delete the found log files? (y/n) " confirm
 
         if [[ "$confirm" == "y" ]]; then
-            rm -i $log_files
-        elif [[ "$confirm" == "n" ]]; then
+    	    archive_file="$log_dir/archive/logs-$(date +%Y%m%d).tar.gz"	
+	    mkdir -p "$log_dir/archive"
+            tar -czf "$archive_file" $log_files
+            echo "Files archived to: $archive_file"
+
+	    for file in $log_files; do
+		rm -i "$file"
+	    done
+	    echo "Selected log files deleted."	
+
+	elif [[ "$confirm" == "n" ]]; then
             echo "Process was canceled."
         else
             echo "Invalid input. Please enter 'y' or 'n'."
